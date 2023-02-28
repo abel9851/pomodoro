ca9f17fdcc8f2703f94eefff14612733fdd9de75
HeejunShin <abel9851@gmail.com>
2023-03-01 01:05:50 +0900

    fix: add explicit login code

--- pomodoro/config/settings.py
+++ pomodoro/config/settings.py
@@ -138,3 +138,7 @@ AUTH_USER_MODEL = "users.User"
 
 
 REST_FRAMEWORK = {"DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication")}
+
+SIMPLE_JWT = {
+    "TOKEN_OBTAIN_SERIALIZER": "users.serializers.UserLoginTokenObtainPairSerializer",
+}
--- pomodoro/users/views.py
+++ pomodoro/users/views.py
@@ -1,5 +1,7 @@
-from rest_framework import response, status
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .serializers import UserLoginTokenObtainPairSerializer
 
@@ -9,11 +11,12 @@ class UserApiLogin(TokenObtainPairView):
     """simple jwt를 상속해서 커스텀한 login view"""
 
     def post(self, request):
+        serializer = UserLoginTokenObtainPairSerializer(data=request.data)
 
-        serializer = UserLoginTokenObtainPairSerializer(request.data)
-
-        if serializer.is_valid():
-            return response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            # TODO 메시지 추가
-            return response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e.args[0])
+        except Exception as e:
+            return Response({"msg": str(e)}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.validated_data, status=status.HTTP_200_OK)
