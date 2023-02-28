dc5d3fee1f232d379b4bd137f09e5afd0d1a3cd9
HeejunShin <abel9851@gmail.com>
2023-02-28 09:11:09 +0900

    feat: add login feature

--- pomodoro/users/views.py
+++ pomodoro/users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import response, status
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# Create your views here.
+from .serializers import UserLoginTokenObtainPairSerializer
+
+
+class UserApiLogin(TokenObtainPairView):
+
+    """simple jwt를 상속해서 커스텀한 login view"""
+
+    def post(self, request):
+
+        serializer = UserLoginTokenObtainPairSerializer(request.data)
+
+        if serializer.is_valid():
+            return response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            # TODO 메시지 추가
+            return response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
