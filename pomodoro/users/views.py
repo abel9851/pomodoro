6dd93b3493e095c3ec05aae3f3f4b0e3820a67e4
HeejunShin <abel9851@gmail.com>
2023-03-05 13:28:21 +0900

    fix: modify logout view

--- pomodoro/users/views.py
+++ pomodoro/users/views.py
@@ -2,13 +2,15 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
-from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from .serializers import UserLoginTokenObtainPairSerializer
+from .serializers import UserLoginTokenObtainPairSerializer, UserLogoutSerializer
 
 
 class UserApiLogin(TokenObtainPairView):
+    pass
+    # get_authenticat_header()를 사용하지 않으면 GenericAPIView랑 같으니까 안쓴다면 GenericAPIview로 바꾸기
+    # genericAPIView가 뭔지 조사하기 exception자동으로 걸러주는지 확인할것.
 
     """simple jwt를 상속해서 커스텀한 login view"""
 
@@ -18,20 +20,27 @@ class UserApiLogin(TokenObtainPairView):
         try:
             serializer.is_valid(raise_exception=True)
         except TokenError as e:
-            raise InvalidToken(e.args[0])
-        except Exception as e:
-            return Response({"msg": str(e)}, status=status.HTTP_401_UNAUTHORIZED)
+            raise InvalidToken(e)
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
 
 class UserApiLogout(APIView):
+    """jwt를 black list에 추가해서 로그아웃"""
+
+    # drf 인증,인가 강의보고 permission class 이해하고 추가하기
+
     def post(self, request):
 
-        refresh_token = request.COOKIES.get("refresh_token")
-        if refresh_token is not None:
-            token = RefreshToken(refresh_token)
-            token.blacklist()
+        serializer = UserLogoutSerializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except TokenError as e:
+            raise InvalidToken(e)
 
-        response = Response()
-        response.delete_cookie("refresh_token")
+        response = Response(status=status.HTTP_200_OK)
+
+        # cookie에서 Rresh token 삭제
+        # front에서는 bearer schema에 있는 access token을 삭제할 필요가 있다.
+        response.delete_cookie("refresh")
+
         return response
