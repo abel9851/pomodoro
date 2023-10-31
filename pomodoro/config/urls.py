1029fa36c48629a13cc0fd4483ed2bafe04b3c8e
HeejunShin <abel9851@gmail.com>
2023-10-31 21:03:30 +0900

    feat: set up django debug toolbar

--- pomodoro/config/urls.py
+++ pomodoro/config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/users/", include("users.urls")),
     path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    path("__debug__", include("debug_toolbar.urls")),
 ]
