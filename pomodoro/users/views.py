002dae0f98446e8087ba69769d34e9b27f0dda55
HeejunShin <abel9851@gmail.com>
2023-11-04 16:28:27 +0900

    chore: comment to do for future

--- pomodoro/users/views.py
+++ pomodoro/users/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .serializers import UserLoginTokenObtainPairSerializer, UserLogoutSerializer
 
@@ -13,8 +14,9 @@ class UserApiLogin(APIView):
     # genericAPIView가 뭔지 조사하기 exception자동으로 걸러주는지 확인할것.
     # 로그인 자체는 session login이 필요하다. 왜냐하면 JWT Authentication을 하면
     # username, password와 같은 credential이 아닌, token을 날려야하기 때문.
-    authentication_classes = []
-    permission_classes = []
+    # openapi로 수정한 다음에는 JWT인증으로 하자.
+    authentication_classes = [SessionAuthentication]
+    permission_classes = [IsAuthenticated]
     # AllowAny를 해놓으면 authentication을 어떻게 해놓던간에 그 api에 request를 제한없이 날릴수 있다.
     # authentication_classes를 지정하면 그 api는 식별을 authentication_class에 있는 방식으로 식별한다.
     # 그 다음 permission_classes에 IsAutenticated를 하면 request.user의 is_authenticated 메소드가 True인지 확인한다.
