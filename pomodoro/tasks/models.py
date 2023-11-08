80eca7e3024339102d945278f5a6cf69e586aba6
HeejunShin <abel9851@gmail.com>
2023-11-08 23:29:02 +0900

    feat: add due_date field

--- pomodoro/tasks/models.py
+++ pomodoro/tasks/models.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from core import models as core_models
 
 from django.db import models
@@ -13,10 +15,11 @@ class Task(core_models.TimeStampedModel):
         LOW = 2, _("LOW")
 
     name = models.CharField(verbose_name="task name", max_length=200)
-    memo = models.CharField(verbose_name="task memo", max_length=300)
+    memo = models.CharField(
+        verbose_name="task memo", max_length=300, default="", blank=True
+    )
     # TODO: db에 저장되는 건 integer, client에 표시되는 것은 글자가 표시되도록 하기. -> frontend에서 직접 작성해야한다.
     priority = models.IntegerField(choices=Priority.choices, default=Priority.MEDIUM)
-    # TODO due_date
-    # due_date = models.DateTimeField()
+    due_date = models.DateField(default=date.today)
 
     # TODO: repeat 기능 추가
