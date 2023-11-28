abb6312538f97fbdcd11f4b35642e43fd270b7a5
HeejunShin <abel9851@gmail.com>
2023-11-29 00:54:29 +0900

    feat: add Pomodoro data in Task when create Task

--- pomodoro/tasks/serializers.py
+++ pomodoro/tasks/serializers.py
@@ -1,4 +1,5 @@
 from pomodoros.models import Pomodoro
+from pomodoros.serializers import PomodoroCreateSerializer
 from rest_framework import serializers
 
 from .models import Task
@@ -7,10 +8,12 @@ from .models import Task
 class TaskPomodoroCreateSerializer(serializers.ModelSerializer):
     """Task Create model Serializer"""
 
-    pomodoro_count = serializers.IntegerField(max_value=42, min_value=1)
+    pomodoro_count = serializers.IntegerField(
+        max_value=42, min_value=1, write_only=True
+    )
     # TODO: Pomodoro의 데이터도 같이 response할 수 있도록 필드 추가하기 231124
     # https://www.django-rest-framework.org/api-guide/relations/
-    # pomodoros = serializers.
+    pomodoros = PomodoroCreateSerializer(many=True, read_only=True)
 
     class Meta:
         model = Task
@@ -23,3 +26,6 @@ class TaskPomodoroCreateSerializer(serializers.ModelSerializer):
         Pomodoro.objects.bulk_create(pomodoros)
 
         return task
+
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
