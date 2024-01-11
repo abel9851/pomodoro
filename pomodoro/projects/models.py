17b96c989c4106c0fb1562b97bd6b460e3a92201
HeejunShin <66953834+abel9851@users.noreply.github.com>
2024-01-12 00:25:31 +0900

    fix: change project task model relationship

--- pomodoro/projects/models.py
+++ pomodoro/projects/models.py
@@ -18,12 +18,6 @@ class Project(core_models.TimeStampedModel):
         related_name="projects",
         on_delete=models.CASCADE,
     )
-    tasks = models.ManyToManyField(
-        "tasks.Task",
-        verbose_name="related tasks",
-        related_name="projects",
-        through="ProjectTaskAssociation",
-    )
     description = models.CharField(
         verbose_name="project description", max_length=300, default="", blank=True
     )
@@ -31,23 +25,3 @@ class Project(core_models.TimeStampedModel):
         verbose_name="color code", max_length=150, default="#0067c0"
     )
     is_active = models.BooleanField(verbose_name="project 활성화 여부")
-
-
-class ProjectTaskAssociation(core_models.TimeStampedModel):
-    """Project Task Association Model Definition"""
-
-    class Meta:
-        db_table = "project_task_associations"
-
-    project = models.ForeignKey(
-        "Project",
-        verbose_name="related project",
-        related_name="task_association",
-        on_delete=models.CASCADE,
-    )
-    task = models.ForeignKey(
-        "tasks.Task",
-        verbose_name="related task",
-        related_name="project_association",
-        on_delete=models.CASCADE,
-    )
