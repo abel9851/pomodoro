7cea3de743fe0899f63a29cd0e04b82a1323d4a6
HeejunShin <66953834+abel9851@users.noreply.github.com>
2024-01-30 22:41:11 +0900

    chore: change openapi endpoint name

--- pomodoro/config/urls.py
+++ pomodoro/config/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path("api/v1/", include("api.urls", namespace="api")),
     path("api/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
     path("__debug__", include("debug_toolbar.urls")),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
